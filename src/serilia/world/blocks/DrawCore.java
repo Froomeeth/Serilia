0b9ac563b1ebb7d9a0eea3dc4645b0623937207c
FrooMeeth <116809073+Froomeeth@users.noreply.github.com>
2024-03-04 22:02:31 +0100

    some glow changes

--- src/serilia/world/blocks/DrawCore.java
+++ src/serilia/world/blocks/DrawCore.java
@@ -1,16 +1,19 @@
 package serilia.world.blocks;
 
 import arc.Core;
-import arc.graphics.Blending;
 import arc.graphics.Color;
-import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.TextureRegion;
+import arc.math.Mathf;
+import arc.util.Time;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
 import mindustry.world.blocks.storage.CoreBlock;
 
 public class DrawCore extends CoreBlock{
     public float powerProduction = 0;
-    public Color glowColor = Color.red;
+    public Color[] glowColors = {Color.valueOf("00000000"), Color.red, Color.red, Color.pink, Color.acid, Color.sky};
     public TextureRegion glow;
+    public float glowMag = 0.5f, glowScl = 10f;
 
     public DrawCore(String name){
         super(name);
@@ -23,26 +26,14 @@ public class DrawCore extends CoreBlock{
         glow = Core.atlas.find(name + "-glow");
     }
 
-    public class NullCoreBuild extends CoreBuild{
+    public class DrawCoreBuild extends CoreBuild{
         @Override
         public float getPowerProduction(){
             return powerProduction;
         }
         @Override
-        public void draw(){
-            super.draw();
-            if(!glow.found()) return;
-
-            //set color and blend mode
-            Draw.blend(Blending.additive);
-            Draw.color(glowColor);
-
-            //draw sprite
-            Draw.rect(glow, x, y);
-
-            //reset both (((very important)))
-            Draw.blend();
-            Draw.color();
+        public void draw(){super.draw();
+            Drawf.additive(glow, team.id < 6 ? glowColors[team.id] : glowColors[1], 1f - glowMag + Mathf.absin(Time.time, glowScl, glowMag), x, y, 0f, Layer.blockAdditive);
         }
     }
 }
