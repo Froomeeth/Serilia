57523fcf4cf54668a7c9e7afff2bfcfdc366bff5
nullevoy <89076920+stacktrace-error@users.noreply.github.com>
2024-03-05 19:09:03 +0100

    sploosh

--- src/serilia/SeriliaMain.java
+++ src/serilia/SeriliaMain.java
@@ -1,13 +1,23 @@
 package serilia;
 
+import arc.math.Mathf;
+import mindustry.Vars;
 import mindustry.mod.*;
 import serilia.content.AndarvosBlocks;
 import serilia.gen.*;
 
 public class SeriliaMain extends Mod{
+    public static String[] sploosh = {
+            "Do 15% of a backflip!",
+            "why is it always thinking",
+            "been"
+    };
+
     @Override
     public void loadContent(){
         EntityRegistry.register();
         AndarvosBlocks.load();
+
+        Vars.mods.getMod("serilia").meta.subtitle = sploosh[Mathf.random(sploosh.length - 1)];
     }
 }
